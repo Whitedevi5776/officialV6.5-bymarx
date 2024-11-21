568ce80ed408c3b8194210e45ed093557c6205de
Whitedevi5776 <149379248+Whitedevi5776@users.noreply.github.com>
2024-11-21 16:21:01 +0100

    Add files via upload

---
+++ slide/adiwConfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'approval': {
+    'num': '6283137225821',
+    'text': "setujui",
+    'set': "👀 Harus mendapatkan persetujuan oleh creator script, Jika ingin memakai script ini",
+    'greet': "*Disetujui Oleh Creator, Silahkan Restart Panel Atau Run Ulang script* ☠️ "
+  },
+  'creatorScript': '6283137225821',
+  'filePath': "./slide/vocado.co",
+  'checkFilePath': "../avocado.js",
+  'codeToDetect': 'main();'
+};
